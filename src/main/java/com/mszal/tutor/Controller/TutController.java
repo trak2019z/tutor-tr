4814aedb9fb5ee5624904261a0cc6a5194f7288d
mszal1 <43968108+mszal1@users.noreply.github.com>
2018-11-07 23:15:47 +0100

    v1.2
    Dodanie do projektu kodu javascript wykorzystującego imgur api do przesyłania zdjęć na hosting bezpośrednio z poziomu aplikacji.

--- src/main/java/com/mszal/tutor/Controller/TutController.java
+++ src/main/java/com/mszal/tutor/Controller/TutController.java
@@ -43,4 +43,8 @@ public class TutController {
       }
         return "tut";
     }
+    @GetMapping("/addtutorial")
+    public String addTutorial(Model model){
+        return "addtutorial";
+    }
 }
